b039dbf829d990f0a2c6e8507933807fc06593c9
SynGameDev <syndicategamedev@gmail.com>
2020-06-04 03:23:15 +0000

    (OPT #1) Display Error When unable to find terminal

--- Assets/Scripts/ConnectToTerminal.cs
+++ Assets/Scripts/ConnectToTerminal.cs
@@ -9,9 +9,12 @@ public class ConnectToTerminal : MonoBehaviour
     [Header("Application Input Fields")]
     [SerializeField] private TMP_InputField _EnteredAddress;                       // IP Address Entered
 
+    [SerializeField] private GameObject _ErrorText;
+
     [SerializeField] private GameObject _SuccessText;
     [SerializeField] private GameObject _ConnectButton;
     [SerializeField] private float _WaitTime;
+    
     public void ValidateTerminalConnecting() {
 
         Debug.Log(GameController.Instance.ActiveContract);
@@ -19,6 +22,8 @@ public class ConnectToTerminal : MonoBehaviour
         if(_EnteredAddress.text == GameController.Instance.GetActiveContract().TerminalIP) {
             SceneController.Instance.OpenTerminalConnector();
             CloseApp();
+        } else {
+            _ErrorText.SetActive(true);
         }
     }
 
