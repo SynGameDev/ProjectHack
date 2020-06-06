5a447c9b6e10e91efe473e3ac90c74700ffc7302
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-07 02:37:09 +1000

    Open Terminal

--- Assets/Scripts/ApplicationControllers/OpenApplications.cs
+++ Assets/Scripts/ApplicationControllers/OpenApplications.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class OpenApplications : MonoBehaviour
+public class OpenApplications : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private string _ApplicationToOpen;
+    [SerializeField] private TextMeshProUGUI _Text;
+
+    public void OnPointerClick(PointerEventData pointerEventData) {
+        OpenApplication();
+    }
+
+    private void Start() {
+       // _ApplicationToOpen = _Text.text;
+    }
 
     public void OpenApplication() {
-        switch(_ApplicationToOpen) {
+        var app = GetComponentInChildren<TextMeshProUGUI>().text;
+        Debug.Log(app);
+        switch(app) {
             case "Software Center":
                 OpenSoftwareCenter();
+                break;
+            case "AceXTerminal":
+                SceneController.Instance.OpenAceXTerminal();
                 break;
 
         }
