a93313d2d1f468143b5cd0a320eae71859249fd9
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-06 23:34:54 +1000

    Added Cracked Applications

---
+++ Assets/Scripts/Controllers/ApplicationDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationDatabase : MonoBehaviour
+{
+    public static ApplicationDatabase Instance;
+
+    [SerializeField] private List<ScriptableObject> _SoftwareApplications = new List<ScriptableObject>();
+    [SerializeField] private List<ScriptableObject> _CrackedSoftwareApplication = new List<ScriptableObject>();
+
+    [Header("Applications")]
+    public ScriptableObject SoftwareCenter;
+
+    private void Awake() {
+        if(Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public List<ScriptableObject> GetSoftwareApps() => _SoftwareApplications;
+    public List<ScriptableObject> GetCrackedApps() => _CrackedSoftwareApplication;
+}
