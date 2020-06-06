296d7d9ad7f7d101370130273ae6735f33eea987
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-06 18:33:48 +1000

    Count items added

--- Assets/Scripts/ApplicationControllers/SoftwareCenterMainController.cs
+++ Assets/Scripts/ApplicationControllers/SoftwareCenterMainController.cs
@@ -56,14 +56,19 @@ public class SoftwareCenterMainController : MonoBehaviour
                 var Application = app as ApplicationScriptableObject;
                 if(Row_1_Count < MaxItemsInRow) {
                     CreateAppItem(Application, _Row_1);
+                    Row_1_Count += 1;
                 } else if(Row_2_Count < MaxItemsInRow) {
                     CreateAppItem(Application, _Row_2);
+                    Row_2_Count += 1;
                 } else if(Row_3_Count < MaxItemsInRow) {
                     CreateAppItem(Application, _Row_3);
+                    Row_3_Count += 1;
                 } else if(Row_4_Count < MaxItemsInRow) {
                     CreateAppItem(Application, _Row_4);
+                    Row_4_Count += 1;
                 } else if(Row_5_Count < MaxItemsInRow) {
                     CreateAppItem(Application, _Row_5);
+                    Row_5_Count += 1;
                 }
             }
         }
