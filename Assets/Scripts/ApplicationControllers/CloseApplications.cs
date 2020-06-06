204ddc577c5077ce506db54d7406a1567d5940a3
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-06 18:33:31 +1000

    Createed Method to Close Applications

---
+++ Assets/Scripts/ApplicationControllers/CloseApplications.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseApplications : MonoBehaviour
+{
+    [SerializeField] private string _ApplicationToClose;
+
+    public void CloseApp() {
+        switch(_ApplicationToClose) {
+            case "Software Center":
+                SceneController.Instance.CloseSoftwareCenter();
+                break;
+        }
+    }
+}
