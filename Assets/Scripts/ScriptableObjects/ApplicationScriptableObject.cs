c38195a6ddc0db2ad129c1d0d4369e3b706a846d
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-07 02:36:51 +1000

    Updated Scriptable objects

--- Assets/Scripts/ScriptableObjects/ApplicationScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/ApplicationScriptableObject.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName="Application", menuName="Scriptable Objects/Applications/New Application", order = 1)]
 public class ApplicationScriptableObject : ScriptableObject
 {
+
+    [Header("Application Deatils")]
     public string ApplicationName;
     [TextArea]
     public string ApplicationDescription;
     public List<Sprite> ApplicationScreenshots = new List<Sprite>();
-
     public Sprite ApplicationIcon;
-    public MonoBehaviour OpenCloseApp;
 
+    [Header("Server Information")]
+    public bool CrackedApplication;
+    public string ApplicationDownloadURL;
+
+    [Header("Store Information")]
     [Tooltip("Amount to obtain the application")] public int PurchaseAmount;
     [Tooltip("Space the application will take on the HDD")] public int HDDSpace;
 }
