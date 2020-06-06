104e7dc79c8764c461f39133dca5b76dca8cff05
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-06 23:35:49 +1000

    Cracked Application

---
+++ Assets/Scripts/ScriptableObjects/ApplicationScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName="Application", menuName="Scriptable Objects/Applications/New Application", order = 1)]
+public class ApplicationScriptableObject : ScriptableObject
+{
+    public string ApplicationName;
+    [TextArea]
+    public string ApplicationDescription;
+    public List<Sprite> ApplicationScreenshots = new List<Sprite>();
+
+    public Sprite ApplicationIcon;
+    public MonoBehaviour OpenCloseApp;
+
+    [Tooltip("Amount to obtain the application")] public int PurchaseAmount;
+    [Tooltip("Space the application will take on the HDD")] public int HDDSpace;
+}
---
+++ Assets/Scripts/ScriptableObjects/CrackedAppScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName="Cracked App", menuName="Scriptable Objects/Applications/New Cracked Application", order=2)]
+public class CrackedAppScriptableObject : ScriptableObject
+{
+    public string ApplicationName;
+    public Sprite ApplicationIcon;
+    public string ApplicationURL;
+    public int HHD;
+}
