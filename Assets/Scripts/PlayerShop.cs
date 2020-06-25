4b32e2ae74e126917e9e861b66db0ef437c2cf8d
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-06-25 21:55:20 +1000

    Upgrade Objects

--- Assets/Scripts/PlayerShop.cs
+++ Assets/Scripts/PlayerShop.cs
@@ -25,37 +25,80 @@ public class PlayerShop : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _SQLCost;
     [SerializeField] private TextMeshProUGUI _PhishCost;
 
+    [Header("Item Cost")]           // TODO: #1wwy2r
+    [SerializeField] private int _DownloadUpgradeCost;
+    [SerializeField] private int _ContractSpaceUpgradeCost;
+    [SerializeField] private int _ExpireTimerUpgradeCost;
+    [SerializeField] private int _CompleteTimerUpgradeCost;
+    [SerializeField] private int _BruteForceUpgradeCost;
+    [SerializeField] private int _SQLUpgradeCost;
+    [SerializeField] private int _PhishUpgradeCost;
 
-
-    private void Awake() {
-
-    }
+    [Header("Modifier Cost")]
+    [SerializeField, Range(1.5f, 3)] private float _UpgradeModifer;
 
     private void Start() {
         _Player = GameController.Instance.GetPlayerData();
     }
 
-    public void UpgradeDownloadSpeed() {
-        _Player.DownloadSpeedMutliplier += 0.3f;         // TODO: Create a method to increase the data.
+    private void Update() {
+        CreateTextData();
+        SetItemCost();
     }
 
-    public void UpgradeContractSpaces() {
-        _Player.ContractSpace += 5;
+    private void CreateTextData() {
+        _DownloadLevel.text = GetDownloadLevel().ToString();
+        _ContractSpaceLevel.text = GetContractSpactes().ToString();
+        _ExpireTimerLevel.text = GetExpireLevel().ToString();
+        _CompleteTimerLevel.text = GetCompleteLevel().ToString();
+        _BruteForceLevel.text = GetBruteForceLevel().ToString();
+        _SQLLevel.text = GetSQLLevel().ToString();
+        _PhishLevel.text = GetPhish().ToString();
     }
 
-    public void UpgradeExpireTimer() {
-        // TODO: Create a method to upgrade timer
+    private void SetItemCost() {
+        _DownloadCost.text = _DownloadUpgradeCost.ToString();
+        _ContractSpaceCost.text = _ContractSpaceUpgradeCost.ToString();
+        _ExpireTimerCost.text = _ExpireTimerUpgradeCost.ToString();
+        _CompleteTimerCost.text = _CompleteTimerUpgradeCost.ToString();
+        _BruteForceCost.text = _BruteForceUpgradeCost.ToString();
+        _SQLCost.text = _SQLUpgradeCost.ToString();
+        _PhishCost.text = _PhishCost.ToString();
     }
 
-    public void UpgradeBruteForce() {
-        // TODO
+    // Item Cost Mod
+
+    public void UpgradeDownloadLevel() {
+        _Player.DownloadLevel+= 1;
+        _DownloadUpgradeCost = ApplyIncrease(_DownloadUpgradeCost);
     }
 
-    public void UpgradeSQL() {
-        // TODO
+    public void UpgradeContractSpace() {
+        _Player.ContractSpaces += 5;
+        _ContractSpaceUpgradeCost = ApplyIncrease(_ContractSpaceUpgradeCost);
     }
 
-    public void UpgradePhish() {
-        // TODO
+    public void UpgradeExpireLevel() {
+        _Player.ExpireTimerLevel += 1;
+        _ExpireTimerUpgradeCost = ApplyIncrease(_ExpireTimerUpgradeCost);
     }
+
+    public void UpgradeCompleteLevel() {
+        _Player.CompleteTimerLevel += 1;
+        _CompleteTimerUpgradeCost = ApplyIncrease(_CompleteTimerUpgradeCost);
+    }
+
+    public int ApplyIncrease(int ToUpgrade) {
+        return Mathf.RoundToInt(ToUpgrade * _UpgradeModifer);
+    }
+
+
+    // Get the item cost
+    public int GetDownloadLevel() => _Player.DownloadLevel;
+    public int GetContractSpactes() => _Player.ContractSpaces;
+    public int GetExpireLevel() => _Player.ExpireTimerLevel;
+    public int GetCompleteLevel() => _Player.CompleteTimerLevel;
+    public int GetBruteForceLevel() => _Player.BruteForceLevel;
+    public int GetSQLLevel() => _Player.SQLLevel;
+    public int GetPhish() => _Player.PhishLevel;
 }
