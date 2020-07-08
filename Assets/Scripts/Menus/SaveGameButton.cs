df39d952d2368a86e7158b35f909e3ef08b7808a
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-07-08 23:40:42 +1000

    Backlog Task ##1yva4m

---
+++ Assets/Scripts/Menus/SaveGameButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGameButton : MonoBehaviour
+{
+    public void SaveGame()
+    {
+        SaveGameSystem.Instance.SaveGame(GameController.Instance.GetPlayerData().PlayerName);
+    }
+}
