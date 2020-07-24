deb8b41b5a0de4d2e838022ba42c1bf3d988a530
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-07-24 17:32:19 +1000

    Start Terminal Generation

---
+++ Assets/Scripts/Systems/Generation/GenerateTerminal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerminal 
+{
+    public static TerminalInfo GenerateContract()
+    {
+        return CreateTerminal("Desktop");
+    }
+
+    private static TerminalInfo CreateTerminal(string type)
+    {
+        TerminalInfo NewTerminal = new TerminalInfo();                // Create the new terminal
+        
+        
+        NewTerminal.TerminalIP = GenerateIP();
+        NewTerminal.TerminalType = type;
+        NewTerminal.TemrinalName = GenerateTerminalName(type);
+        NewTerminal.HHD = UnityEngine.Random.Range(2, 230);
+    }
+
+    private static string GenerateIP()
+    {
+        string num_1 = UnityEngine.Random.Range(10, 1000).ToString();
+        string num_2 = UnityEngine.Random.Range(100, 1000).ToString();
+        string num_3 = UnityEngine.Random.Range(100, 1000).ToString();
+
+        return num_1 + "." + num_2 + "." + num_3;
+    }
+
+    private static string GenerateTerminalName(string type)
+    {
+        return "";
+    }
+
+}
