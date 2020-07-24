ff18e909b2cd9070d7fdca7f051044315074df98
syndicategamedev <60543821+syndicategamedev@users.noreply.github.com>
2020-07-24 17:28:03 +1000

    Singeltons

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = (T) FindObjectOfType(typeof(T));
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
